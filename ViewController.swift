2acbc1a8d677f40a58ef33a330e450e9d61669c1
Stephanie Chiu <37680110+stephaniechiu@users.noreply.github.com>
2018-12-19 14:55:56 -0800

    Add files via upload
    Initial Commit. UI created

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Stephanie on 12/19/18.
+//  Copyright © 2018 Stephanie Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
